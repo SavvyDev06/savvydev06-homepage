4c7996623c17b8267644b370b254920e838f3018
Savvy <developer.savvasmica@gmail.com>
2022-03-01 15:16:36 +0200

    remove unused import

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
