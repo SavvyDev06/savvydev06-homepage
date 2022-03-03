fac6a7c22a2f318da4727586b2a7f2d3371aaf07
Savvy <developer.savvasmica@gmail.com>
2022-03-03 22:53:49 +0200

    Add profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image } from '@chakra-ui/react'
 
 const Page = () => {
   const paragraphStyleObj = {
@@ -21,6 +21,23 @@ const Page = () => {
           <p style={paragraphStyleObj}>
             Digital Creator ( Software &amp; Game Developer / Music Producer )
           </p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/savvy.png"
+            alt="My Profile Image!"
+          />
         </Box>
       </Box>
     </Container>
