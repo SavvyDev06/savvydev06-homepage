a60c6a57a5d2ab5878d03467a298cbf7a04fdcf4
SavvyDev06 <39233671+SavvyDev06@users.noreply.github.com>
2022-03-03 16:49:06 +0200

    Added Titillium Web Font & fixed syntax error

--- components/fonts.js
+++ components/fonts.js
@@ -4,7 +4,7 @@ const Fonts = () => (
   <Global
     styles={`
         @import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap');
         `}
   />
 )
--- components/navbar.js
+++ components/navbar.js
@@ -55,7 +55,7 @@ const Navbar = props => {
         justify="space-between"
       >
         <Flex align="center" mr={5}>
-          <Heading as="h1" size="lg" letterSPacing={'tighter'}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
             <Logo />
           </Heading>
         </Flex>
