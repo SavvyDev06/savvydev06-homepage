2159c0bf07ed1fa1b1c31e56c4dd724a5d853db0
Savvy <developer.savvasmica@gmail.com>
2022-03-30 21:59:08 +0300

    Add page transitions and improved projects page

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ import ParagraphBio from '../components/paragraphBio'
 import ParagraphHeading from '../components/paragraphHeading'
 import CursiveHeading from '../components/cursiveHeading'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import { BioSection, BioYear } from '../components/bio'
 import HeadsUpToast from '../components/headsUpToast'
 
@@ -61,261 +62,263 @@ const Page = () => {
   }
 
   return (
-    <Container>
-      <HeadsUpToast />
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue(
-          'linear-gradient(45deg, #73C6E8, #A5FF8D)',
-          'linear-gradient(45deg, #6C28C4, #73C6E8)'
-        )} /* whiteAlpha.200 */
-        p={3}
-        mb={6}
-        align="center"
-      >
-        <CursiveHeading>Hello,</CursiveHeading>
-        <ParagraphHeading style={introStyleObj}>
-          I&apos;m Savvy, a student enterpreneur from Cyprus!
-        </ParagraphHeading>
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" fontWeight="500" variant="page-title">
-            Savvas M. Agisilaou
-          </Heading>
-          <ParagraphHeading>
-            Digital Creator ( Software &amp; Game Developer / Music Producer )
-          </ParagraphHeading>
-        </Box>
+    <Layout>
+      <Container>
+        <HeadsUpToast />
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue(
+            'linear-gradient(45deg, #73C6E8, #A5FF8D)',
+            'linear-gradient(45deg, #6C28C4, #73C6E8)'
+          )} /* whiteAlpha.200 */
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/savvy.png"
-            alt="My Profile Image!"
-          />
+          <CursiveHeading>Hello,</CursiveHeading>
+          <ParagraphHeading style={introStyleObj}>
+            I&apos;m Savvy, a student enterpreneur from Cyprus!
+          </ParagraphHeading>
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading fontWeight="600" as="h3" variant="section-title">
-          Work
-        </Heading>
-        <ParagraphMain>
-          Savvas – better known as Savvy – is a full-stack software developer
-          with a passion for creating useful software, to help improve our daily
-          lives through the power of code.
-          <br />
-          He often finds himself engineering hardware as well, such as printed
-          circuit boards, 3D Prints / CAD, and regularly tinkers with Arduino
-          and Raspberry Pi boards to make all sorts of cool stuff.
-          <br />
-          Some of Savvy’s most notable software projects include{' '}
-          <NextLink href="/projects/degore" scroll={false} passHref>
-            <Link>DeGore</Link>
-          </NextLink>
-          ,{' '}
-          <NextLink href="/projects/tfc" scroll={false} passHref>
-            <Link>The Five Chapters</Link>
-          </NextLink>{' '}
-          and{' '}
-          <NextLink href="/projects/scpu" scroll={false} passHref>
-            <Link>SCP United</Link>
-          </NextLink>
-          , as well as the{' '}
-          <NextLink href="/projects/qckboard" scroll={false} passHref>
-            <Link>QckBoard Macro+</Link>
-          </NextLink>{' '}
-          on the hardware side of things.
-        </ParagraphMain>
-        <Box align="center" my={4}>
-          <NextLink href="/projects" passHref>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="purple">
-              My Portfolio
-            </Button>
-          </NextLink>
+
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" fontWeight="500" variant="page-title">
+              Savvas M. Agisilaou
+            </Heading>
+            <ParagraphHeading>
+              Digital Creator ( Software &amp; Game Developer / Music Producer )
+            </ParagraphHeading>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/savvy.png"
+              alt="My Profile Image!"
+            />
+          </Box>
         </Box>
-      </Section>
-
-      <Section delay={0.2}>
-        <Heading fontWeight="600" as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>2004</BioYear>
-          <FontAwesomeIcon icon={faCakeCandles} /> Born in Nicosia, Cyprus.
-        </BioSection>
-        <BioSection>
-          <BioYear>2018</BioYear>
-          <FontAwesomeIcon icon={faLaptopCode} /> Passed ECDL C++ Programming
-          Course with distinction
-        </BioSection>
-        <BioSection>
-          <BioYear>20??</BioYear>
-          <FontAwesomeIcon icon={faAnglesRight} /> More to be added
-        </BioSection>
-        <BioSection>
-          <BioYear>2021-present</BioYear>
-          <FontAwesomeIcon icon={faRightToBracket} /> Joined the team at SCP
-          United as a programmer
-        </BioSection>
-        <BioSection>
-          <BioYear>2021-present</BioYear>
-          <FontAwesomeIcon icon={faRocket} /> Founded DeGore, and work as a lead
-          developer
-        </BioSection>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading fontWeight="600" as="h3" variant="section-title">
-          Languages
-        </Heading>
-        <ParagraphBio>
-          <FontAwesomeIcon icon={faCheckToSlot} />{' '}
-          <b>
-            <u>Knowledgeable</u>
-          </b>
-          <ParagraphLeftPadded>
-            <FontAwesomeIcon icon={faJsSquare} /> JavaScript
-            <br />
-            <FontAwesomeIcon icon={faGears} /> TypeScript
-            <br />
-            <FontAwesomeIcon icon={faC} /> C++
-            <br />
-            <FontAwesomeIcon icon={faHashtag} /> C# (dotnet)
-            <br />
-            <FontAwesomeIcon icon={faPython} /> Python
-            <br />
-            <FontAwesomeIcon icon={faHtml5} /> HTML&amp;CSS
-          </ParagraphLeftPadded>
-          <FontAwesomeIcon icon={faGlasses} />{' '}
-          <b>
-            <u>Learning</u>
-          </b>{' '}
+        <Section delay={0.1}>
+          <Heading fontWeight="600" as="h3" variant="section-title">
+            Work
+          </Heading>
           <ParagraphMain>
-            <FontAwesomeIcon icon={faGears} /> TypeScript <br />
+            Savvas – better known as Savvy – is a full-stack software developer
+            with a passion for creating useful software, to help improve our
+            daily lives through the power of code.
+            <br />
+            He often finds himself engineering hardware as well, such as printed
+            circuit boards, 3D Prints / CAD, and regularly tinkers with Arduino
+            and Raspberry Pi boards to make all sorts of cool stuff.
+            <br />
+            Some of Savvy’s most notable software projects include{' '}
+            <NextLink href="/projects/degore" scroll={false} passHref>
+              <Link>DeGore</Link>
+            </NextLink>
+            ,{' '}
+            <NextLink href="/projects/tfc" scroll={false} passHref>
+              <Link>The Five Chapters</Link>
+            </NextLink>{' '}
+            and{' '}
+            <NextLink href="/projects/scpu" scroll={false} passHref>
+              <Link>SCP United</Link>
+            </NextLink>
+            , as well as the{' '}
+            <NextLink href="/projects/qckboard" scroll={false} passHref>
+              <Link>QckBoard Macro+</Link>
+            </NextLink>{' '}
+            on the hardware side of things.
           </ParagraphMain>
-          <FontAwesomeIcon icon={faCalendarCheck} />{' '}
-          <b>
-            <u>Planned to Learn</u>
-          </b>{' '}
-          <ParagraphLeftPadded>
-            <FontAwesomeIcon icon={faRust} /> Rust <br />
-            <FontAwesomeIcon icon={faGolang} /> Go
-          </ParagraphLeftPadded>
-        </ParagraphBio>
-      </Section>
+          <Box align="center" my={4}>
+            <NextLink href="/projects" passHref>
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="purple">
+                My Portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
 
-      <Section delay={0.4}>
-        <Heading fontWeight="600" as="h3" variant="section-title">
-          Frameworks
-        </Heading>
-        <ParagraphBio>
-          <FontAwesomeIcon icon={faCheckToSlot} />{' '}
-          <b>
-            <u>Knowledgeable</u>
-          </b>
-          <ParagraphLeftPadded>
-            <FontAwesomeIcon icon={faReact} /> React
-            <br />
-            <FontAwesomeIcon icon={faN} /> NextJS
-            <br />
-            <FontAwesomeIcon icon={faAtom} /> Electron
-            <br />
-            <FontAwesomeIcon icon={faVuejs} /> NuxtJS
-          </ParagraphLeftPadded>
-          <FontAwesomeIcon icon={faGlasses} />{' '}
-          <b>
-            <u>Learning</u>
-          </b>{' '}
-          <ParagraphLeftPadded>
-            <FontAwesomeIcon icon={faFeather} /> Flutter
-          </ParagraphLeftPadded>
-          <FontAwesomeIcon icon={faCalendarCheck} />{' '}
-          <b>
-            <u>Planned to Learn</u>
-          </b>{' '}
-          <ParagraphMain>
-            <FontAwesomeIcon icon={faMicrosoft} /> Xamarin
-          </ParagraphMain>
-        </ParagraphBio>
-      </Section>
+        <Section delay={0.2}>
+          <Heading fontWeight="600" as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>2004</BioYear>
+            <FontAwesomeIcon icon={faCakeCandles} /> Born in Nicosia, Cyprus.
+          </BioSection>
+          <BioSection>
+            <BioYear>2018</BioYear>
+            <FontAwesomeIcon icon={faLaptopCode} /> Passed ECDL C++ Programming
+            Course with distinction
+          </BioSection>
+          <BioSection>
+            <BioYear>20??</BioYear>
+            <FontAwesomeIcon icon={faAnglesRight} /> More to be added
+          </BioSection>
+          <BioSection>
+            <BioYear>2021-present</BioYear>
+            <FontAwesomeIcon icon={faRightToBracket} /> Joined the team at SCP
+            United as a programmer
+          </BioSection>
+          <BioSection>
+            <BioYear>2021-present</BioYear>
+            <FontAwesomeIcon icon={faRocket} /> Founded DeGore, and work as a
+            lead developer
+          </BioSection>
+        </Section>
 
-      <Section delay={0.5}>
-        <Heading fontWeight="600" as="h3" variant="section-title">
-          Technologies (Tech Stacks)
-        </Heading>
-        <ParagraphBio>
-          <FontAwesomeIcon icon={faCheckToSlot} />{' '}
-          <b>
-            <u>Knowledgeable</u>
-          </b>
-          <ParagraphLeftPadded>
-            <FontAwesomeIcon icon={faDocker} /> Docker
-            <br />
-            <FontAwesomeIcon icon={faDatabase} /> PostgreSQL
-            <br />
-            <FontAwesomeIcon icon={faLeaf} /> MongoDB
-            <br />
-            <FontAwesomeIcon icon={faFeatherPointed} /> Apache Server (more to
-            be added)
-          </ParagraphLeftPadded>
-          <FontAwesomeIcon icon={faCalendarCheck} />{' '}
-          <b>
-            <u>Planned to Learn</u>
-          </b>{' '}
-          <ParagraphLeftPadded>
-            <FontAwesomeIcon icon={faShapes} /> Redis
-          </ParagraphLeftPadded>
-        </ParagraphBio>
-      </Section>
+        <Section delay={0.3}>
+          <Heading fontWeight="600" as="h3" variant="section-title">
+            Languages
+          </Heading>
+          <ParagraphBio>
+            <FontAwesomeIcon icon={faCheckToSlot} />{' '}
+            <b>
+              <u>Knowledgeable</u>
+            </b>
+            <ParagraphLeftPadded>
+              <FontAwesomeIcon icon={faJsSquare} /> JavaScript
+              <br />
+              <FontAwesomeIcon icon={faGears} /> TypeScript
+              <br />
+              <FontAwesomeIcon icon={faC} /> C++
+              <br />
+              <FontAwesomeIcon icon={faHashtag} /> C# (dotnet)
+              <br />
+              <FontAwesomeIcon icon={faPython} /> Python
+              <br />
+              <FontAwesomeIcon icon={faHtml5} /> HTML&amp;CSS
+            </ParagraphLeftPadded>
+            <FontAwesomeIcon icon={faGlasses} />{' '}
+            <b>
+              <u>Learning</u>
+            </b>{' '}
+            <ParagraphMain>
+              <FontAwesomeIcon icon={faGears} /> TypeScript <br />
+            </ParagraphMain>
+            <FontAwesomeIcon icon={faCalendarCheck} />{' '}
+            <b>
+              <u>Planned to Learn</u>
+            </b>{' '}
+            <ParagraphLeftPadded>
+              <FontAwesomeIcon icon={faRust} /> Rust <br />
+              <FontAwesomeIcon icon={faGolang} /> Go
+            </ParagraphLeftPadded>
+          </ParagraphBio>
+        </Section>
 
-      <Section delay={0.6}>
-        <Heading fontWeight="600" as="h3" variant="section-title">
-          Software
-        </Heading>
-        <ParagraphBio>
-          <FontAwesomeIcon icon={faCheckToSlot} />{' '}
-          <b>
-            <u>Knowledgeable</u>
-          </b>
-          <ParagraphLeftPadded>
-            <FontAwesomeIcon icon={faCarrot} /> FL Studio
-            <br />
-            <FontAwesomeIcon icon={faMusic} /> Ableton Live
-            <br />
-            <FontAwesomeIcon icon={faClapperboard} /> Adobe Premiere Pro (more
-            to be added)
-          </ParagraphLeftPadded>
-        </ParagraphBio>
-      </Section>
+        <Section delay={0.4}>
+          <Heading fontWeight="600" as="h3" variant="section-title">
+            Frameworks
+          </Heading>
+          <ParagraphBio>
+            <FontAwesomeIcon icon={faCheckToSlot} />{' '}
+            <b>
+              <u>Knowledgeable</u>
+            </b>
+            <ParagraphLeftPadded>
+              <FontAwesomeIcon icon={faReact} /> React
+              <br />
+              <FontAwesomeIcon icon={faN} /> NextJS
+              <br />
+              <FontAwesomeIcon icon={faAtom} /> Electron
+              <br />
+              <FontAwesomeIcon icon={faVuejs} /> NuxtJS
+            </ParagraphLeftPadded>
+            <FontAwesomeIcon icon={faGlasses} />{' '}
+            <b>
+              <u>Learning</u>
+            </b>{' '}
+            <ParagraphLeftPadded>
+              <FontAwesomeIcon icon={faFeather} /> Flutter
+            </ParagraphLeftPadded>
+            <FontAwesomeIcon icon={faCalendarCheck} />{' '}
+            <b>
+              <u>Planned to Learn</u>
+            </b>{' '}
+            <ParagraphMain>
+              <FontAwesomeIcon icon={faMicrosoft} /> Xamarin
+            </ParagraphMain>
+          </ParagraphBio>
+        </Section>
 
-      <Section delay={0.7}>
-        <Heading fontWeight="600" as="h3" variant="section-title">
-          I 💜
-        </Heading>
-        <ParagraphBio>
-          Coding,{' '}
-          <Link href="https://unitedweco.de/degore" target="_blank">
-            Artificial Intelligence / Machine Learning
-          </Link>
-          , PCB Design, CAD, Music (Digital Composition &amp; Piano), Aerospace,
-          Podcasts,{' '}
-          <Link href="https://blog.unitedweco.de" target="_blank">
-            Blogging
-          </Link>
-        </ParagraphBio>
-      </Section>
-    </Container>
+        <Section delay={0.5}>
+          <Heading fontWeight="600" as="h3" variant="section-title">
+            Technologies (Tech Stacks)
+          </Heading>
+          <ParagraphBio>
+            <FontAwesomeIcon icon={faCheckToSlot} />{' '}
+            <b>
+              <u>Knowledgeable</u>
+            </b>
+            <ParagraphLeftPadded>
+              <FontAwesomeIcon icon={faDocker} /> Docker
+              <br />
+              <FontAwesomeIcon icon={faDatabase} /> PostgreSQL
+              <br />
+              <FontAwesomeIcon icon={faLeaf} /> MongoDB
+              <br />
+              <FontAwesomeIcon icon={faFeatherPointed} /> Apache Server (more to
+              be added)
+            </ParagraphLeftPadded>
+            <FontAwesomeIcon icon={faCalendarCheck} />{' '}
+            <b>
+              <u>Planned to Learn</u>
+            </b>{' '}
+            <ParagraphLeftPadded>
+              <FontAwesomeIcon icon={faShapes} /> Redis
+            </ParagraphLeftPadded>
+          </ParagraphBio>
+        </Section>
+
+        <Section delay={0.6}>
+          <Heading fontWeight="600" as="h3" variant="section-title">
+            Software
+          </Heading>
+          <ParagraphBio>
+            <FontAwesomeIcon icon={faCheckToSlot} />{' '}
+            <b>
+              <u>Knowledgeable</u>
+            </b>
+            <ParagraphLeftPadded>
+              <FontAwesomeIcon icon={faCarrot} /> FL Studio
+              <br />
+              <FontAwesomeIcon icon={faMusic} /> Ableton Live
+              <br />
+              <FontAwesomeIcon icon={faClapperboard} /> Adobe Premiere Pro (more
+              to be added)
+            </ParagraphLeftPadded>
+          </ParagraphBio>
+        </Section>
+
+        <Section delay={0.7}>
+          <Heading fontWeight="600" as="h3" variant="section-title">
+            I 💜
+          </Heading>
+          <ParagraphBio>
+            Coding,{' '}
+            <Link href="https://unitedweco.de/degore" target="_blank">
+              Artificial Intelligence / Machine Learning
+            </Link>
+            , PCB Design, CAD, Music (Digital Composition &amp; Piano),
+            Aerospace, Podcasts,{' '}
+            <Link href="https://blog.unitedweco.de" target="_blank">
+              Blogging
+            </Link>
+          </ParagraphBio>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/projects.js
+++ pages/projects.js
@@ -1,88 +1,127 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { ProjectGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 
 import thumbDeGore from '../public/images/projects/degore-thumbnail.png'
 import thumbSCPU from '../public/images/projects/scpu-thumbnail.png'
 import thumbTFC from '../public/images/projects/tfc-thumbnail.png'
 import thumbSolarSystem from '../public/images/projects/solarsystem-thumbnail.jpg'
 import thumbONABLC from '../public/images/projects/onablc-thumbnail.png'
+import thumbDeGoreDash from '../public/images/projects/degore-dashboard-thumbnail.png'
+import thumbDankAPI from '../public/images/projects/dank-api-thumbnail.png'
+import thumbQckBoard from '../public/images/projects/qckboard-macro-plus-thumbnail.png'
 
 import HeadsUpToast from '../components/headsUpToast'
 
 const Projects = () => {
   return (
-    <Container>
-      <HeadsUpToast />
-      <Heading fontWeight="600" as="h3" fontSize={20} mb={4}>
-        Projects
-      </Heading>
-
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <ProjectGridItem id="degore" title="DeGore" thumbnail={thumbDeGore}>
-            The most advanced AI-Powered image moderation solution for
-            communities on the Discord Platform
-          </ProjectGridItem>
-        </Section>
-
-        <Section delay={0.1}>
-          <ProjectGridItem
-            id="tfc"
-            title="The Five Chapters"
-            thumbnail={thumbTFC}
-          >
-            An immersive 3D horror experience.
-          </ProjectGridItem>
-        </Section>
-      </SimpleGrid>
-
-      <Section delay={0.2}>
-        <Divider my={6} />
+    <Layout>
+      <Container>
+        <HeadsUpToast />
         <Heading fontWeight="600" as="h3" fontSize={20} mb={4}>
-          Collaborations
+          Projects
         </Heading>
-      </Section>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <ProjectGridItem id="scpu" title="SCP United" thumbnail={thumbSCPU}>
-            A faithful remaster of the SCP Containment Breach game using
-            elements and systems from the cancelled SCP Unity game.
-          </ProjectGridItem>
-        </Section>
-      </SimpleGrid>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <ProjectGridItem id="degore" title="DeGore" thumbnail={thumbDeGore}>
+              The most advanced AI-Powered image moderation solution for
+              communities on the Discord Platform
+            </ProjectGridItem>
+          </Section>
 
-      <Section delay={0.4}>
-        <Divider my={6} />
-        <Heading fontWeight="600" as="h3" fontSize={20} mb={4}>
-          Past Projects
-        </Heading>
-      </Section>
+          <Section delay={0.1}>
+            <ProjectGridItem
+              id="tfc"
+              title="The Five Chapters"
+              thumbnail={thumbTFC}
+            >
+              An immersive 3D horror experience.
+            </ProjectGridItem>
+          </Section>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.2}>
+            <ProjectGridItem
+              id="degore-dashboard"
+              title="DeGore Dashboard"
+              thumbnail={thumbDeGoreDash}
+            >
+              A sleek and advanced web dashboard to conveniently interface with
+              and configure DeGore.
+            </ProjectGridItem>
+          </Section>
+
+          <Section delay={0.3}>
+            <ProjectGridItem
+              id="dankpods-api"
+              title="DankPods API"
+              thumbnail={thumbDankAPI}
+            >
+              An Express REST API used to get random funny quotes from dankpods
+              videos.
+            </ProjectGridItem>
+          </Section>
+
+          <Section delay={0.4}>
+            <ProjectGridItem
+              id="qckboard-macro-plus"
+              title="QckBoard Macro Plus"
+              thumbnail={thumbQckBoard}
+            >
+              An Express REST API used to get random funny quotes from dankpods
+              videos.
+            </ProjectGridItem>
+          </Section>
+        </SimpleGrid>
+
         <Section delay={0.5}>
-          <ProjectGridItem
-            id="solarsystem"
-            title="Our Solar System"
-            thumbnail={thumbSolarSystem}
-          >
-            An app designed to teach you all about our Solar System.
-          </ProjectGridItem>
+          <Divider my={6} />
+          <Heading fontWeight="600" as="h3" fontSize={20} mb={4}>
+            Collaborations
+          </Heading>
         </Section>
 
-        <Section delay={0.6}>
-          <ProjectGridItem
-            id="onablc"
-            title="One Night at Baldi's Literature Club"
-            thumbnail={thumbONABLC}
-          >
-            Combining Baldi&apos;s Basics, Doki Doki Literature Club, and Five
-            Nights At Freddy&apos;s into one singleplayer horror experience.
-          </ProjectGridItem>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.6}>
+            <ProjectGridItem id="scpu" title="SCP United" thumbnail={thumbSCPU}>
+              A faithful remaster of the SCP Containment Breach game using
+              elements and systems from the cancelled SCP Unity game.
+            </ProjectGridItem>
+          </Section>
+        </SimpleGrid>
+
+        <Section delay={0.7}>
+          <Divider my={6} />
+          <Heading fontWeight="600" as="h3" fontSize={20} mb={4}>
+            Past Projects
+          </Heading>
         </Section>
-      </SimpleGrid>
-    </Container>
+
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.8}>
+            <ProjectGridItem
+              id="solarsystem"
+              title="Our Solar System"
+              thumbnail={thumbSolarSystem}
+            >
+              An app designed to teach you all about our Solar System.
+            </ProjectGridItem>
+          </Section>
+
+          <Section delay={0.9}>
+            <ProjectGridItem
+              id="onablc"
+              title="One Night at Baldi's Literature Club"
+              thumbnail={thumbONABLC}
+            >
+              Combining Baldi&apos;s Basics, Doki Doki Literature Club, and Five
+              Nights At Freddy&apos;s into one singleplayer horror experience.
+            </ProjectGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
