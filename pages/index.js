0e083ad0bbabe6cd11b2404a6009d5b1c8af939c
SavvyDev06 <developer.savvasmica@gmail.com>
2026-02-10 20:42:12 +0000

    Change &amp; to &apos;

--- pages/index.js
+++ pages/index.js
@@ -391,7 +391,7 @@ const Page = () => {
                 <BioSection>
                   <BioYear>2025</BioYear>
                   <FontAwesomeIcon icon={faServer} /> Interned at Edinburgh
-                  University&amp;s Parallel Computing Centre (EPCC)
+                  University&apos;s Parallel Computing Centre (EPCC)
                 </BioSection>
                 <BioSection>
                   <BioYear>2025-present</BioYear>
